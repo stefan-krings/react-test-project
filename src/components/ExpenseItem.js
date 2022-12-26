905018bebac598c36db931f2dcedfa47e92d648c
Stefan Krings <krings-stefan1@web.de>
2022-12-26 23:35:58 +0100

    added props with dummy data to the expense item

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date().toLocaleDateString();
-  const expenseTitle = "Test expense";
-  const expenseAmount = 100.51;
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{`${expenseAmount} €`}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{`${props.amount} €`}</div>
       </div>
     </div>
   );
