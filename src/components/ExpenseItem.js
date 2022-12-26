c99f2575b3615b8ca24d344c99975c2ceecf6556
Stefan Krings <krings-stefan1@web.de>
2022-12-26 23:13:43 +0100

    added dynamic output to the expense item

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date().toLocaleDateString();
+  const expenseTitle = "Test expense";
+  const expenseAmount = 100.51;
   return (
-    <div className='expense-item'>
-      <div>Date</div>
-      <div className='expense-item__description'>
-        <h2>Title</h2>
-        <div className='expense-item__price'>Amount</div>
+    <div className="expense-item">
+      <div>{expenseDate}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{`${expenseAmount} €`}</div>
       </div>
     </div>
   );
